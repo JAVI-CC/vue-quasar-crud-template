bc0a95638a05874e4705d0f7ac22070ddec32c2b
JAVI-CC <javileg98@hotmail.com>
2024-01-20 16:03:32 +0100

    change configuration websockets soketi

--- src/modules/common/services/LaravelEchoPusher/laravelEchoPusherConfig.ts
+++ src/modules/common/services/LaravelEchoPusher/laravelEchoPusherConfig.ts
@@ -8,8 +8,14 @@ const laravelEchoPusherConfig = async (): Promise<Echo> => {
     authEndpoint: `${process.env.APP_AXIOS_BASEURL}/broadcasting/auth`,
     broadcaster: 'pusher',
     key: process.env.PUSHER_APP_KEY,
-    cluster: 'eu',
-    forceTLS: true,
+    wsHost: process.env.PUSHER_APP_HOST,
+    wsPort: process.env.PUSHER_APP_PORT,
+    wssPort: process.env.PUSHER_APP_PORT,
+    forceTLS: false,
+    encrypted: true,
+    disableStats: true,
+    cluster: 'mt1',
+    enabledTransports: ['ws', 'wss'],
     auth: {
       headers: {
         authorization: LocalStorage.getItem('token'),
